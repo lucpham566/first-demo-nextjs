208882a962bd2ff72c7bbed7784c25cfb3defa15
lực phạm <luc.pt@thienthaitechnology.vn>
2020-07-02 11:46:50 +0700

    commit in new branch

---
+++ pages/user.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class user extends Component {
+    render() {
+        return (
+            <div>
+                helo new branch
+            </div>
+        );
+    }
+}
+
+export default user;
